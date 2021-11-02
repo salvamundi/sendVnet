08eebe0c404c03a2112553c65e5794e6b7124633
salvamundi <92865493+salvamundi@users.noreply.github.com>
2021-11-02 17:29:21 +0100

    Update receiver.py

--- src/receiver.py
+++ src/receiver.py
@@ -8,7 +8,7 @@ BUFFER_SIZE = 4096
 opt_s = ha.handle_args()
 
 def receiver(s_ip, s_pt, s_ph):
-    s_ip = opt_s.receiver_ip #ip could be 0.0.0.0 so that way if our machine is i.e 192.168.0.4 and 10.0.0.5 at the same time, we will be able to listen at all the IPv4 addresses, not only with the given one
+    s_ip = opt_s.receiver_ip #ip could be 0.0.0.0 so that way if our machine is i.e 192.168.0.4 and 10.0.0.5 at the same time, we will be able to listen at all those addresses, not only with the given one
     s_pt = opt_s.receiver_port
     s_ph = opt_s.ddir
 
